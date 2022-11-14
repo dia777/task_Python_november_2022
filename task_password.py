ad4105cd1f3a58d0d62db6c7c0d4f091f6eb7cdf
dia777 <Selmi.Murana@jala.university>
2022-11-14 14:49:38 -0400

    primera_task

---
+++ task_password.py
@@ -0,0 +1,11 @@
+﻿import random
+
+mayus = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+minus = "abcdefghijklmnñopqrstuvwxyz"
+num = "0123456789"
+
+all=mayus+minus+num
+
+for i in range (1,random.randint(9,16)):
+    character_randon = random.choice(all)
+    print(character_randon)
